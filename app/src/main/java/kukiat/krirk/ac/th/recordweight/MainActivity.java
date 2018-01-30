b9e76fdd4bd6939076f08f355702566586005b83
Kukiat <Kukiat@gmail.com>
2018-01-30 12:01:14 +0700

    Finish 30Jan

--- app/src/main/java/kukiat/krirk/ac/th/recordweight/MainActivity.java
+++ app/src/main/java/kukiat/krirk/ac/th/recordweight/MainActivity.java
@@ -3,11 +3,19 @@ package kukiat.krirk.ac.th.recordweight;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import kukiat.krirk.ac.th.recordweight.utility.MyManager;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MyManager myManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        myManager = new MyManager(MainActivity.this);
+
+
+    } // Main Method
+} // Main Class
