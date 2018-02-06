5239fc4bd2bc9685a127b03df0774b70c3a11bf1
Kukiat <Kukiat@gmail.com>
2018-02-06 12:05:50 +0700

    Finish 6Feb

--- app/src/main/java/kukiat/krirk/ac/th/recordweight/MainActivity.java
+++ app/src/main/java/kukiat/krirk/ac/th/recordweight/MainActivity.java
@@ -1,9 +1,11 @@
 package kukiat.krirk.ac.th.recordweight;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import kukiat.krirk.ac.th.recordweight.utility.MyManager;
+import kukiat.krirk.ac.th.recordweight.utility.MyOpenHelperSQLite;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,10 +20,22 @@ public class MainActivity extends AppCompatActivity {
 
 
 //        Test Add User
+//        testAddUser();
+
+//        Delete All userTABLE
+//        deleteAllUserTABLE();
+
+    } // Main Method
+
+    private void deleteAllUserTABLE() {
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelperSQLite.databaseName,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete("userTABLE", null, null);
+    }
+
+    private void testAddUser() {
         myManager.addValueToUserTABLE("nick", "start",
                 "weight", "height", "gender",
                 "age");
-
-
-    } // Main Method
+    }
 } // Main Class
